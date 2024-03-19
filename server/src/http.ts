f366f36e1c45da5a6c2cffebf64fb4642c1a7280
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-18 22:55:12 -0300

    feat: create routers

--- server/src/http.ts
+++ server/src/http.ts
@@ -1,12 +1,25 @@
 import cors from "cors";
-import express from "express";
+import express, { Router } from "express";
 import http from "http";
 
 import { Server } from "socket.io";
+import { CreateUserController } from "./controllers/user/create-user";
+import { CreateConnection } from "./controllers/connection/create-connection";
+import { AuthUserController } from "./controllers/user/auth-user";
 
+const router = Router();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//Routers user
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
+
+//Routers connection
+router.post("/connection", new CreateConnection().handle);
+
+app.use(router);
 
 const serverHttp = http.createServer(app);
 
