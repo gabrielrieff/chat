e6cb2ff0abb12872897b6324fcaf3f165b095238
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-25 23:45:26 -0300

    feat: add type

---
+++ client/src/@types/connection/index.d.ts
@@ -0,0 +1,11 @@
+export type Connection = {
+  id: string;
+  phone: string;
+  name: string;
+  photo: null | string;
+  isUser: false | true;
+  created_at: string;
+  update_at: string;
+  userId: string;
+  conversationId: null | string;
+};
