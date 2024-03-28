22be63071a945226314d211a25de33ff45889ce7
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-27 22:26:53 -0300

    refactor

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -1,42 +1,41 @@
-import { io } from "./http";
+import { Server } from "socket.io";
 
-type RoomUser = {
-  socket_id: string;
-  userName: string;
-  room: string;
+const io = new Server(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
 };
 
-type messegeUser = {
-  socket_id: string;
-  userName: string;
-  room: string;
-  messege: string;
-  color: string;
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
 };
 
-const users: Array<RoomUser> = [];
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 
 io.on("connection", (socket) => {
-  socket.on("select_room", (data) => {
-    socket.join(data.room);
+  console.log("User connected");
 
-    const userInRoom = users.find(
-      (user) => user.userName === data.userName && user.room === data.room
-    );
-
-    if (userInRoom) {
-      userInRoom.socket_id = socket.id;
-    } else {
-      users.push({
-        socket_id: socket.id,
-        userName: data.userName,
-        room: data.room,
-      });
-    }
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
   });
 
-  socket.on("messege", (data: messegeUser) => {
-    io.to(data.room).emit("receive_messege", data);
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
   });
 
   socket.on("disconnect", (reason) => {
