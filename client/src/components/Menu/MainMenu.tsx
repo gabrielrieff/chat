d74aaca4d52781d5a54e9bd64d5fe0c608da174d
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-04-04 20:24:14 -0300

    feat: adicionado componente Menu

---
+++ client/src/components/Menu/MainMenu.tsx
@@ -0,0 +1,46 @@
+import {
+  BsFillChatLeftTextFill,
+  BsFillPersonLinesFill,
+  BsFillPersonPlusFill,
+  BsSliders,
+} from "react-icons/bs";
+import { LuLogOut } from "react-icons/lu";
+
+import { Button } from "../ui/button";
+import { Popover, PopoverTrigger } from "../ui/popover";
+import { PopoverNewConnections } from "./PopoverNewConnections";
+
+export function MenuMain() {
+  return (
+    <div className="flex flex-col justify-between h-full p-2 bg-white text-zinc-400">
+      <div className="flex flex-col gap-4">
+        <Button variant={"ghost"}>
+          <BsFillChatLeftTextFill size={28} />
+        </Button>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant={"ghost"}>
+              <BsFillPersonPlusFill size={28} />
+            </Button>
+          </PopoverTrigger>
+          <PopoverNewConnections />
+        </Popover>
+
+        <Button variant={"ghost"}>
+          <BsFillPersonLinesFill size={28} />
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <Button variant={"ghost"}>
+          <BsSliders size={28} />
+        </Button>
+
+        <Button variant={"ghost"}>
+          <LuLogOut size={28} />
+        </Button>
+      </div>
+    </div>
+  );
+}
